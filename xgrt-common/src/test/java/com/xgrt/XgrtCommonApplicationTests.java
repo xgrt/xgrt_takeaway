566e2fcfeb7696128082bb7a96c81a48959a83de
xgrt <2725751476@qq.com>
2024-07-07 16:54:48 +0800

    外卖项目代码

---
+++ xgrt-common/src/test/java/com/xgrt/XgrtCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XgrtCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ xgrt-server/src/test/java/com/xgrt/XgrtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XgrtServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
